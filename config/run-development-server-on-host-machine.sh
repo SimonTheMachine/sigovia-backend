080f29526cb97fa94dd1b62f77f659335e78bca2
SimonTheMachine <simon@alsbirk.com>
2024-10-22 11:16:25 +0200

    Better naming of shell script

--- config/run-development-server-on-host-machine.sh
+++ config/run-development-server-on-host-machine.sh
@@ -3,6 +3,9 @@
 # Provided that the local database is running using: 
 # docker-compose -f config/docker-compose.yml up -d local-db
 
+# Run this script using: 
+# ./config/run-development-server-on-host-machine.sh
+
 # Read and export the environment variables from the .env file in the config folder
 while IFS= read -r line || [ -n "$line" ]; do
   if [[ ! "$line" =~ ^# && "$line" =~ .*=.* ]]; then
